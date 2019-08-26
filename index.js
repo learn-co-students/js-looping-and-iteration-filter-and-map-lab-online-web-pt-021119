0dd7ae3b82cde2c9ccce0265ab05d8bf4c4a54d1
github username <github email address>
2019-08-25 21:20:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver){
+    return driver.revenue > revenue
+  })
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return drivers
+      .filter(function (driver){
+      return driver.revenue > revenue
+      })
+      .map(function(driver){
+        return driver.name
+      })
+}
