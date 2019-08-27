1c48484e88d0200f73a31ff0232282220e5e10b4
cyantis <cyantis@gmail.com>
2019-08-26 19:17:39 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here:
+function driversWithRevenueOver(arr, rev){
+  return arr.filter( function (i) {return i.revenue > rev } );
+}
+
+function driverNamesWithRevenueOver(arr, rev){
+  const revArr = driversWithRevenueOver(arr, rev);
+  const nameArr = [];
+  for(let i in revArr){
+    nameArr.push(revArr[i].name);
+  }
+  return nameArr;
+}
