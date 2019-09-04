06627add74533fcf14635605b4f2a593606bca20
Eyal Toledano <eutait@gmail.com>
2019-09-04 15:17:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here:
+
+function driversWithRevenueOver(collection, number) {
+  return collection.filter(function(element) {
+    return element.revenue > number;
+  });
+};
+
+function driverNamesWithRevenueOver(collection, number) {
+  const drivers = driversWithRevenueOver(collection, number);
+  return drivers.map(function(driver) {
+    return driver.name
+  });
+};
+
+function exactMatch(collection, query) {
+  return collection.filter(function(element) {
+    return element[Object.keys(query)[0]] == query[Object.keys(query)[0]];
+  });
+};
+
+function exactMatchToList(collection, query) {
+  const drivers = exactMatch(collection, query);
+  return drivers.map(function(driver) {
+    return driver.name;
+  });
+};
