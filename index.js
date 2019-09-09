1eb3bbc3c9504d0d7f37c85c3d33373a26e083ea
cavu757 <jose.pehovazdiez@gmail.com>
2019-09-09 16:33:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here:
+function driversWithRevenueOver(array, amount){
+  let newArray = array.filter(driver => driver['revenue']>amount);
+  return newArray;
+};
+
+function driverNamesWithRevenueOver(array, amount){
+  let newArray = driversWithRevenueOver(array, amount)
+  let answer = newArray.map(driver => driver['name']);
+  return answer;
+};
+
+function exactMatch(array, attribute){
+  let tempKey = Object.keys(attribute)[0];
+  let tempValue = Object.values(attribute)[0];
+  let newArray = array.filter(driver => driver[tempKey]===tempValue);
+  return newArray;
+};
+
+function exactMatchToList(array, attribute){
+  let newArray = exactMatch(array, attribute)
+  let answer = newArray.map(driver => driver['name']);
+  return answer;
+};
