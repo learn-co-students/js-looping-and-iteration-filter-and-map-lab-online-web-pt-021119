52536fa42c0b1f3caf79311faac9807a05a95620
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-09-14 16:46:13 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here:
+function driversWithRevenueOver(driver, num){
+  return driver.filter(function(element){
+    return element.revenue > num;
+  });
+}
+
+function driverNamesWithRevenueOver(drivers, num){
+  const newDrivers = driversWithRevenueOver(drivers, num);
+  return newDrivers.map(function(element){
+    return element.name;
+  });
+}
+
+function exactMatch(drivers, query){
+  return drivers.filter(function(element){
+    return element[Object.keys(query)[0]] == query[Object.keys(query)[0]];
+  });
+}
+
+function exactMatchToList(drivers, query) {
+  const newDrivers = exactMatch(drivers, query);
+  return newDrivers.map(function(driver){
+    return driver.name;
+  });
+};
